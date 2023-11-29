ae414b055fb521cf8711ef7e1f82109e38d58896
TMA\dbhoang <dbhoang@tma.com.vn>
2023-11-29 17:29:19 +0700

    add script to run batch job

--- generate_metrics.sh
+++ generate_metrics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "my_metric 42" | curl --data-binary @- http://pushgateway:9091/metrics/job/my_batch_job
+echo "my_metric 42" | curl --data-binary @- http://pushgateway:9091/metrics/job/pushgateway
