1051822af28c5b92b102903808f2bf7d0bfda246
TMA\dbhoang <dbhoang@tma.com.vn>
2023-12-05 21:54:27 +0700

    change target

--- generate_metrics.sh
+++ generate_metrics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "my_metric 42" | curl -u admin:kmaAt16i@  --data-binary @- http://54.169.111.153:9091/metrics/job/pushgateway
+echo "my_metric 42" | curl -u admin:kmaAt16i@  --data-binary @- http://13.229.236.123:9091/metrics/job/pushgateway
